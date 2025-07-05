e198b5b6c932806636cff4ff0549bdf6eacb6b32
zarmalwal <zarmalwalabdulwali1@gmail.com>
2025-07-04 22:29:22 -0400

    WEEK1

---
+++ index.html/scripts/getdates.js
@@ -0,0 +1,4 @@
+// Show current year
+document.getElementById("current-year").textContent = new Date().getFullYear();
+// Show last modified date
+document.getElementById("last-modified").textContent = "Last Modified: " + document.lastModified;
